bb19f21f8d7bc0220817d651ca83e4adcdd08977
Nitish <nitishapp9455@gmail.com>
2023-12-25 23:58:16 +0530

    first commit

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const app = express();
 const dotenv=require('dotenv');
-const {MongoClient} = require("mongodb");
-const Port = process.env.Port || 8000;
-const cors = require("cors")
+const Port = process.env.PORT || 8000;
+const cors = require("cors");
 const mongoose=require('mongoose');
 const articleData = require('./model');
 const router=express.Router()
@@ -31,30 +30,7 @@ mongoose.connect("mongodb+srv://shivaevilking:1234shivansh@cluster0.lhsukan.mong
 .catch((error)=>{
     console.log('Error occured : '+error);
 })
-    // try {
-        
-    // const client = await MongoClient.connect("mongodb://127.0.0.1")
-    // const db = client.db("mernblog");
-    // await operation(db);
-    // client.close();
-    // } catch (error) {
-    //     res.status(500).json({message:"error connecting to database", error})
-    // }
-//}
-// app.get("/api/articles/:name", async (req,res) => {
-//     withDB(async (db)=>{
-//         const artilename = req.params.name;
-   
-    
-//     const articlesinfo = await db
-//     .collection("articles")
-//     .findOne({name: artilename});
-//     res.status(200).json(articlesinfo);
-//     },res)
-        
-    
-    
-//})
+
 router.post('/api/articles/:name/add-comments', async(req,res) => {
     const {username, text} = req.body;
     const articleName = req.params.name;
